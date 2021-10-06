7f97557493f390829b4c80324ec0bb110ed01f0d
karan <karanrana@billericak12.com>
2021-10-05 23:10:44 -0400

    Charts

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,14 @@
-
+package com.company;
 
 public class Main {
+    public Main(){
+
+    }
+
 
     public static void main(String[] args) {
 	// write your code here
+        PollDisplayPanel p1= new PollDisplayPanel("Jeoff", "Norbert","Bob");
+        System.out.println(p1);
     }
 }
